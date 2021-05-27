b571a7caabec94b4d0d555523c5395ae2fa9ec5f
yolkCat <80354983+yolkCat@users.noreply.github.com>
2021-05-27 23:19:54 +0300

    add neural network dependencies

---
+++ functions/ModelGen.m
@@ -0,0 +1,31 @@
+function [netS,netD] = ModelGen(Traindata,SBPTarget,DBPTarget)
+
+inputs = Traindata;
+targets = DBPTarget;
+
+% Create a Fitting Network
+hiddenLayerSize = 10;
+net = fitnet(hiddenLayerSize);
+
+% Set up Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+ 
+% Train the Network
+[netD,tr] = train(net,inputs,targets);
+
+targets = SBPTarget;
+% Create a Fitting Network
+hiddenLayerSize = 10;
+net = fitnet(hiddenLayerSize);
+
+% Set up Division of Data for Training, Validation, Testing
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+ 
+% Train the Network
+[netS,tr] = train(net,inputs,targets);
+
+end
